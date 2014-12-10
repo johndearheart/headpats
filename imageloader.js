77a61a7b3fb74fdd15d36b1a0bca614b803195c3
John Dearheart <ollin@tyndall.local>
2014-12-10 11:09:01 -0800

    Non-image links are now skipped
    [Including gfycats :( ]

--- imageloader.js
+++ imageloader.js
@@ -1,30 +1,40 @@
 function loadImages(subreddit, container, timeout) {
 	//Load some hot posts from the specified subreddit
-	reddit.hot(subreddit).fetch(function(res) {
-	    console.log(res.data);
-		
+	reddit.hot(subreddit).fetch(function(res) {		
 		var max = res.data.children.length;
 		var i = 0;
 		//Update the container with each post
 		window.setInterval( function() {
-				//store the post in a variable
+				//Update to the next post
 				var post = res.data.children[i].data;
 				var link = post.url;
 			
-				var imageRegex = /.+\.(jpg|png|gif)/;
-				//if there's an image, load it into the container
-				if (imageRegex.test(link)) {
-					container.css('background-image', 'url(' + link + ')');
-					console.log(link);
-					console.log(i);
+				//Skip all posts that aren't images
+				while (!isImageLink(link)) {
+					i++;
+					post = res.data.children[i].data;
+					link = post.url;
 				}
 				
+				//Load the images				
+				container.css('background-image', 'url(' + link + ')');
+				console.log(link);
+				console.log(i);
+				
+				//Increment image counter.  If we're out, go back to the start.
 				i++;
-				if (i > max) {
+				if (i >= max) {
 					i = 0;
 				}
+				
+				//Print for debug
 				console.log(i);
 			}, timeout);
 		}
 	);
+}
+
+function isImageLink(link) {
+	var imageRegex = /.+\.(jpg|png|gif)/;
+	return imageRegex.test(link);
 }
