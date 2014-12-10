b417681d6d6bad3e8dd6f38e9a9b4e5dbf171073
John Dearheart <ollin@tyndall.local>
2014-12-09 18:01:56 -0800

    Added crucial head pat funcitonality

---
+++ imageloader.js
@@ -0,0 +1,30 @@
+function loadImages(subreddit, container) {
+	//Load some hot posts from the specified subreddit
+	reddit.hot(subreddit).fetch(function(res) {
+	    console.log(res.data);
+		
+		var max = res.data.children.length;
+		var i = 0;
+		//Update the container with each post
+		window.setInterval( function() {
+				//store the post in a variable
+				var post = res.data.children[i].data;
+				var link = post.url;
+			
+				var imageRegex = /.+\.(jpg|png|gif)/;
+				//if there's an image, load it into the container
+				if (imageRegex.test(link)) {
+					container.css('background-image', 'url(' + link + ')');
+					console.log(link);
+					console.log(i);
+				}
+				
+				i++;
+				if (i > max) {
+					i = 0;
+				}
+			}, 2000);
+			//timeout
+		}
+	);
+}
